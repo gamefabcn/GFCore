ba4d3a1edaaf3f97368354616e2091b65a793023
game_fab <yingran@live.cn>
2026-03-23 17:36:36 +0800

    Add files via upload
    1.完成Alt + F8打包；2.一键清理。

---
+++ GFCore/GFEditor/GFEditor.Build.cs
@@ -0,0 +1,47 @@
+﻿using UnrealBuildTool;
+
+public class GFEditor : ModuleRules
+{
+    public GFEditor(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        // ====================== 公开依赖（Shipping 打包安全） ======================
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                "CoreUObject",
+                "Engine",
+                "GFRuntime"
+            }
+        );
+
+        // ====================== Editor-only 依赖（关键隔离！） ======================
+        if (Target.Type == TargetRules.TargetType.Editor)
+        {
+            PrivateDependencyModuleNames.AddRange(
+                new string[]
+                {
+                    "ToolMenus",
+                    "InputCore",
+                    "Slate",
+                    "SlateCore",
+                    "EditorStyle",
+                    "UnrealEd",
+                    "LevelEditor",
+                    "AutomationController",
+                    "AutomationTest",
+                    "AssetTools",
+                    "Projects",
+                    "DesktopPlatform",
+                    "EditorScriptingUtilities",
+                    "UATHelper"
+                }
+            );
+
+            PublicDefinitions.Add("WITH_EDITOR=1");
+        }
+        
+    }
+}
---
+++ GFCore/GFRuntime/Private/GFRuntime.cpp
@@ -0,0 +1,17 @@
+﻿#include "GFRuntime.h"
+
+#define LOCTEXT_NAMESPACE "FGFRuntimeModule"
+
+void FGFRuntimeModule::StartupModule()
+{
+    
+}
+
+void FGFRuntimeModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FGFRuntimeModule, GFRuntime)
